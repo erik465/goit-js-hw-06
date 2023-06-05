b0bc220c7a8bab763d761b6d55f637ab93e0b620
erik465 <ehalasz06@gmail.com>
2023-06-05 18:47:39 +0200

    .

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color")
+
+button.addEventListener("click", () => {
+  const color = getRandomHexColor()
+  document.querySelector(".color").innerHTML = color
+  document.querySelector("body").style.backgroundColor = color
+
+})
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,24 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const amountInput = document.querySelector("#controls").firstElementChild
+
+const createButton = document.querySelector('button[data-create]')
+const destroyButton = document.querySelector('button[data-destroy]')
+
+let boxContainer = document.querySelector("#boxes")
+
+createButton.addEventListener("click", () => {
+  boxContainer.innerHTML = ""
+
+  for(let i=1; i<=amountInput.value; i++) {
+    const size = i * 30
+    boxContainer.insertAdjacentHTML("beforeend", `<div style="margin-bottom:10px; width:${size}px; height:${size}px; background:${getRandomHexColor()}"> </div>`)
+  }
+})
+
+destroyButton.addEventListener("click", ()=>{
+  console.log(boxContainer)
+  boxContainer.innerHTML = ""
+})
